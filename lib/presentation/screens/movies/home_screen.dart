e6807a65795b05c3b1756041469dd23ba25b439a
Eduard <dudu.bdn@gmail.com>
2024-01-27 20:06:27 +0100

    check

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -10,7 +10,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text(Environment.theMovieDbKey),
+        child: Text(Environment.theMovieDbKey()),
       ),
     );
   }
